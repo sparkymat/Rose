fe2de2fe1784bcfc670faf1d33318a110d3c9569
Max <csy0013@googlemail.com>
2010-11-03 08:43:34 +0530

    Added copyright header

--- video.h
+++ video.h
@@ -1,3 +1,20 @@
+/*
+* Copyright (c) 2010 Ajith Hussain
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* Ajith Hussain - initial contribution.
+*
+* Contributors:
+*
+* Description:
+*
+*/
+
 #ifndef VIDEO_H
 #define VIDEO_H
 
